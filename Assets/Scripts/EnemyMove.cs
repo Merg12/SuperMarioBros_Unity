dc8db11c9a0535119260fae09a0b4cbed34e5055
Merg <ninetynineknights@gmail.com>
2018-09-06 03:07:07 -0500

    update

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -9,6 +9,8 @@ public class EnemyMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//apparently this raycast shoots out a ray from the very center of the gameobject/player/enemy and whatever other object it touches in the game, you can make it do certain things
+		//
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
 		if(hit.distance < 0.8f)
