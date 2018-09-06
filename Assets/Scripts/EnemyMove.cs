abce6c880d9493a400ebbd047c083e8b99176382
Merg <ninetynineknights@gmail.com>
2018-09-06 02:21:48 -0500

    enemy movement

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+	public int EnemySpeed;
+	public int XMoveDirection;
+	
+	// Update is called once per frame
+	void Update () {
+
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+		
+	}
+}
