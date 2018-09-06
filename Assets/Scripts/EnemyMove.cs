f0b4b657ac35ee8c9a2e267ef2931b8ddf1d8020
Merg <ninetynineknights@gmail.com>
2018-09-06 02:43:21 -0500

    enemy turning once it hits the wall

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -9,8 +9,23 @@ public class EnemyMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+		if(hit.distance < 0.7f)
+		{
+			Flip();
+		}
 		
+	}
+	void Flip()
+	{
+		if(XMoveDirection > 0)
+		{
+			XMoveDirection = -1;
+		}
+		else
+		{
+			XMoveDirection = 1;
+		}
 	}
 }
