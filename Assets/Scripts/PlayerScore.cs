d58e2f8af01363a339acc1f0a3b62447d411ec96
Merg <ninetynineknights@gmail.com>
2018-09-08 23:43:33 -0500

    times up and dies

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScore : MonoBehaviour {
 
-	public float TimeLeft = 120;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	[SerializeField]
+	private float TimeLeft = 30; //MUST be a float; won't work as an int (i tried)
+	public int player_Score = 0;
 	
 	// Update is called once per frame
 	void Update () {
 
 		TimeLeft -= Time.deltaTime; //makes the countdown by seconds
-		Debug.Log(TimeLeft);
+		Debug.Log(TimeLeft); //sees if the countdown is working
+		if(TimeLeft < 0.1f)
+		{
+			SceneManager.LoadScene("SampleScene");
+		}
 		
 	}
 }
