8f740e3e5814a058f2df9765377d24ba2c12fa7f
Merg <ninetynineknights@gmail.com>
2018-09-08 22:29:26 -0500

    Tidying up + added a score recorder

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -12,7 +12,7 @@ public class EnemyMove : MonoBehaviour {
 		//apparently this raycast shoots out a ray from the very center of the gameobject/player/enemy and whatever other object it touches in the game, you can make it do certain things
 		//
 		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XMoveDirection, 0));
-		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed;
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection, 0) * EnemySpeed; //move in a direction
 		if(hit.distance < 0.9f) //this is how far from the enemy to the wall that determines when the enemy should start moving the other direction
 		{
 			Flip();
@@ -23,7 +23,7 @@ public class EnemyMove : MonoBehaviour {
 	{
 		if(XMoveDirection > 0)
 		{
-			XMoveDirection = -1;
+			XMoveDirection = -1; //the negative sign makes anything direction turn opposite
 		}
 		else
 		{
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+
+	public float TimeLeft = 120;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		TimeLeft -= Time.deltaTime; //makes the countdown by seconds
+		Debug.Log(TimeLeft);
+		
+	}
+}
