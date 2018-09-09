16f6d3af80f62a943c92c77be03f475aff588e3b
Merg <ninetynineknights@gmail.com>
2018-09-09 01:08:41 -0500

    now you can see canvas score and time left!

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerScore : MonoBehaviour {
 
 	[SerializeField]
 	private float TimeLeft = 30; //MUST be a float; won't work as an int (i tried)
-	public int player_Score = 0;
+	public int Player_Score = 0;
+	public GameObject TimeLeftUI;
+	public GameObject Player_ScoreUI;
 	
 	// Update is called once per frame
 	void Update () {
 
 		TimeLeft -= Time.deltaTime; //makes the countdown by seconds
-		Debug.Log(TimeLeft); //sees if the countdown is working
+		
+		TimeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)TimeLeft);
+		Player_ScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + Player_Score);
+		//Debug.Log(TimeLeft); //sees if the countdown is working
 		if(TimeLeft < 0.1f)
 		{
 			SceneManager.LoadScene("SampleScene");
 		}
 		
+	}
+
+	void OnTriggerEnter2D(Collider2D trig)
+	{
+		CountScore();
+		Debug.Log("touched the end of the level");
+	}
+
+	void CountScore()
+	{
+		Player_Score += (int)(TimeLeft * 10); //normally you can't change a float back to an int but we cast'd that into an int to make it work for logging
+		Debug.Log("player score: " + Player_Score);
 	}
 }
